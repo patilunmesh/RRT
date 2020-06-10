414275bacde3bd9658a90a63c5854f250fa27347
patilunmesh <patilunmeshrao@gmail.com>
2020-06-10 06:36:34 +0530

    sections

--- rrt.cpp
+++ rrt.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 
-namespace  std;
+using namespace  std;
 
 int main()
 {
-	cout << "hello wotrld";
+	// section 1 parameters
+
+	//section2 init vars
+
+	//input section (obstacles, start pose, goal pose, resolution, grid size)
+
+	// section 3 call in loop random generator
+
+	// section 4 validation n linking
+
+	// section 5 add vertex add node
+
+	// goal check
+
+	//path smoothing
+
 	return 0;
 }
+
+// random generator
+
+
+
+// collision checker
+
+
+
+//add node, add edge
+
+
+
+//path store
+
+
+
+//path smoothing
